a5e02629323cf69815e1f00d4a7e9a3c4a847062
Bijay <sthabeejay2060@gmail.com>
2024-02-18 16:04:07 +0545

    add comment

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 console.log("Welcome to nodeJs")
+//hello commit
 console.log(1+3)
 const name="Bijay";
 console.log(name)
 const a=20;
 const b=20;
-const sum=a+b;
+const c=3;
+const sum=a+c;
 console.log(sum)
